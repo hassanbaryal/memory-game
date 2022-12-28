4e6d444828650831baa89a8c7b8014eed5753836
hassanbaryal <h.baryal2@gmail.com>
2022-12-28 17:08:58 -0500

    Imported header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './header/Header';
 
 function App() {
   return (
     <div className="h-screen flex flex-col overflow-auto">
+      <Header />
     </div>
   );
 }

62366584fa017a041472de35621a2562b9441055
hassanbaryal <h.baryal2@gmail.com>
2022-12-28 17:08:22 -0500

    Created header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="flex justify-center p-4">
+      <h1 className="text-6xl">Memory Game</h1>
+    </div>
+  );
+}
+
+export default Header;

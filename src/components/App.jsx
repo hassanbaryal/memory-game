fa4f6a3de0201502cdc039a5d73b9f7d4aabca42
hassanbaryal <h.baryal2@gmail.com>
2023-01-01 21:20:31 -0500

    Imported Game component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header/Header';
+import Game from './game/Game';
 
 function App() {
   return (
     <div className="h-screen flex flex-col overflow-auto">
       <Header />
+      <Game />
     </div>
   );
 }
